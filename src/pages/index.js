45fcc2fc2a7fe54491810349941d710f63136f57
princekhj555 <princekhj555@gmail.com>
2021-05-25 17:46:35 +0530

    Full homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>AMAZON Clone</title>
       </Head>
       
       {/* Header */}
       <Header/>
+
+      <main className="max-w-screen-2xl mx-auto ">
+        {/* Banner */}
+          <Banner/>
+        {/* Product Feed */}
+        <ProductFeed products={products}/>
+      </main>
     </div>
   );
+}
+export async function getServerSideProps(context){
+  const products=await fetch("https://fakestoreapi.com/products").then(
+    (res)=>res.json()
+  )
+  return{
+    props:{
+      products,
+    },
+  }
 }
